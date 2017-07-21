1f976892da079f29e1caac082249dd378d791426
Jeffrey Noe <noej@flip1.engr.oregonstate.edu>
2017-07-20 21:13:10 -0700

    began inputstring function

--- projects/noej/quiz-2/testme.c
+++ projects/noej/quiz-2/testme.c
@@ -9,10 +9,17 @@ char inputChar()
     return ' ';
 }
 
+// I've verified that this function works and needs randomized.
 char *inputString()
 {
-    // TODO: rewrite this function
-    return "";
+    char* str = malloc(5);
+    str[0] = 'r';
+    str[1] = 'e';
+    str[2] = 's';
+    str[3] = 'e';
+    str[4] = 't';
+
+    return str;
 }
 
 void testme()
@@ -23,27 +30,35 @@ void testme()
   int state = 0;
   while (1)
   {
-    tcCount++;
-    c = inputChar();
-    s = inputString();
-    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+        tcCount++;
+        c = inputChar();
+        s = inputString();
+        printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
 
-    if (c == '[' && state == 0) state = 1;
-    if (c == '(' && state == 1) state = 2;
-    if (c == '{' && state == 2) state = 3;
-    if (c == ' '&& state == 3) state = 4;
-    if (c == 'a' && state == 4) state = 5;
-    if (c == 'x' && state == 5) state = 6;
-    if (c == '}' && state == 6) state = 7;
-    if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8) state = 9;
-    if (s[0] == 'r' && s[1] == 'e'
-       && s[2] == 's' && s[3] == 'e'
-       && s[4] == 't' && s[5] == '\0'
-       && state == 9)
+        if (c == '[' && state == 0)
+            state = 1;
+        if (c == '(' && state == 1)
+            state = 2;
+        if (c == '{' && state == 2)
+            state = 3;
+        if (c == ' '&& state == 3)
+            state = 4;
+        if (c == 'a' && state == 4)
+            state = 5;
+        if (c == 'x' && state == 5)
+            state = 6;
+        if (c == '}' && state == 6)
+            state = 7;
+        if (c == ')' && state == 7)
+            state = 8;
+        if (c == ']' && state == 8)
+            state = 9;
+
+    if (s[0] == 'r' && s[1] == 'e' && s[2] == 's' && s[3] == 'e' && s[4] == 't' && s[5] == '\0' && state == 9)
     {
-      printf("error ");
-      exit(200);
+          printf("error ");
+          free(s);
+          exit(200);
     }
   }
 }
